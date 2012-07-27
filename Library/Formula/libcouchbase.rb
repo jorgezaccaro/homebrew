c0f70403029a969c3d4b691f686adba18a1f72cd
Sergey Avseyev <sergey.avseyev@gmail.com>
2012-07-27 09:20:29 +0300

    libcouchbase preview version 1.1.0dp8

--- Library/Formula/libcouchbase.rb
+++ Library/Formula/libcouchbase.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libcouchbase < Formula
   homepage 'http://couchbase.com/develop/c/next'
-  url 'http://packages.couchbase.com/clients/c/libcouchbase-1.1.0dp7.tar.gz'
-  md5 '0b13a12903ee828207bfa6aed4328568'
+  url 'http://packages.couchbase.com/clients/c/libcouchbase-1.1.0dp8.tar.gz'
+  md5 'b4a01165f7031dd91e01666664cce505'
 
   depends_on 'libevent'
   depends_on 'libvbucket'
