0d4460cdfca21457536855e228e1bb201a1c6712
Sergey Avseyev <sergey.avseyev@gmail.com>
2012-10-12 14:05:25 -0700

    libcouchbase second beta version 2.0.0beta2

--- Library/Formula/libcouchbase.rb
+++ Library/Formula/libcouchbase.rb
@@ -1,16 +1,33 @@
 require 'formula'
 
+def with_libev_plugin?
+  ARGV.include?('--with-libev-plugin')
+end
+
+def without_libevent_plugin?
+  ARGV.include?('--without-libevent-plugin')
+end
+
 class Libcouchbase < Formula
   homepage 'http://couchbase.com/develop/c/next'
-  url 'http://packages.couchbase.com/clients/c/libcouchbase-2.0.0beta.tar.gz'
-  md5 'a0c98ff2ee5c017eae18fea907936fec'
+  url 'http://packages.couchbase.com/clients/c/libcouchbase-2.0.0beta2.tar.gz'
+  md5 'df257b0f11b6bf787108d5f9ab91c439'
 
-  depends_on 'libevent'
+  depends_on 'libevent' unless without_libevent_plugin?
+  depends_on 'libev' if with_libev_plugin?
+
+  def options
+    [
+      ['--with-libev-plugin', 'Build libev IO plugin'],
+      ['--without-libevent-plugin', 'Do not build libev IO plugin']
+    ]
+  end
 
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
+                          "--disable-examples",
                           "--disable-couchbasemock"
     system "make install"
   end
